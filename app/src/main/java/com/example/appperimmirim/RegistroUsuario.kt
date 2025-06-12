447fdc4fde3b5a0d95a515b95efc78cde7667906
BraianPassos <braianpassos408@gmail.com>
2025-06-12 20:44:51 -0300

    Tela de registro de usuário primitiva, pensando em dividir a tela para um melhorar o fluxo dos dados.
    Próximas alterações:
    divisão
    Tela 1 – Informações pessoais:
    Nome completo
    CPF
    Data de nascimento
    Número de telefone
    Endereço
    
    Tela 2 – Acesso e documentos:
    Email
    Senha
    Confirmar senha
    Foto do documento (RG)
    Foto de perfil
    Botão de cadastro

---
+++ app/src/main/java/com/example/appperimmirim/RegistroUsuario.kt
@@ -0,0 +1,9 @@
+package com.example.appperimmirim
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistroUsuario : AppCompatActivity() {
+
+}
